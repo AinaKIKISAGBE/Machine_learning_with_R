115cdbc2699d0ca54a449fa6347173a2fae4d21e
Aina KIKI-SAGBE <40332540+AinaKIKISAGBE@users.noreply.github.com>
2018-09-19 15:36:36 +0200

    Update 4. LogisticPLS.R

--- 4. LogisticPLS.R
+++ 4. LogisticPLS.R
@@ -1,5 +1,5 @@
 #################### Chargement des packages ci-dessous #################### 
-setwd("C:/Users/P.Lhostis/Documents/Master/CLASSIFICATION")
+setwd("D:/projet/")
 getwd()
 #install.packages("broom")
 #install.packages("questionr")
@@ -22,11 +22,11 @@ library(devtools)
 library(factoextra)
 library(FactoMineR)
 library(effects)
-##### Fonctions calcul des corrÈlations et des taux d'erreurs ##### 
+##### Fonctions calcul des corr√©lations et des taux d'erreurs ##### 
 cram = function(x,y) {
   CV = sqrt(chisq.test(x, y, correct=FALSE)$statistic /
               (length(x) * (min(length(unique(x)),length(unique(y))) - 1)))
-  print.noquote("CramÈr V / Phi:")
+  print.noquote("Cram√©r V / Phi:")
   return(as.numeric(CV))
 }
 Tx_err <- function(y, ypred) {
@@ -36,7 +36,7 @@ Tx_err <- function(y, ypred) {
 }
 #############################################################################
 
-### Importation de l'Èchantillon d'apprentissage et de l'Èchantillon test/validation ###
+### Importation de l'√©chantillon d'apprentissage et de l'√©chantillon test/validation ###
 # ECHANTILLON DE VALIDATION 30% #
 ech_valid<-read_csv("C:/Users/P.Lhostis/Documents/Master/CLASSIFICATION/Base_TEST.csv")
 # ECHANTILLON D'APPRENTISSAGE 70%#
@@ -46,9 +46,9 @@ ech_test<-read_csv("C:/Users/P.Lhostis/Documents/Master/CLASSIFICATION/Base_TRAI
 doublons <- which(duplicated(ech_test$cle)) 
 # Aucun doublon dans nos individus #
 
-### On va maintenant sÈlectionner nos variables ‡ l'aide d'un V de Cramer ###
-### Suppression de nos variable quanti, prÈdictions d'une dummy ###
-### On effectue ensuite une sÈlection stepwise sur notre rÈgression logistique ###
+### On va maintenant s√©lectionner nos variables √† l'aide d'un V de Cramer ###
+### Suppression de nos variable quanti, pr√©dictions d'une dummy ###
+### On effectue ensuite une s√©lection stepwise sur notre r√©gression logistique ###
 
 # 1 . Observation de notre variable cible et des variables explicatives ##
 ech_test$y<-as.factor(ech_test$y)
@@ -62,7 +62,7 @@ freq(ech_test$y)
 ech_test$job<-as.factor(ech_test$job)
 levels(ech_test$job)
 freq(ech_test$job)
-#Classes sours reprÈsentÈes => observation classe#
+#Classes sours repr√©sent√©es => observation classe#
 ech_test$cl_job<-as.factor(ech_test$cl_job)
 levels(ech_test$cl_job)
 freq(ech_test$cl_job)
@@ -84,7 +84,7 @@ freq(ech_test$education)
 ech_test$education<-relevel(ech_test$education,"secondary")
 cram(ech_test$y,ech_test$education)
 ## V=0.07557
-## Sous reprÈsentation de la classe unknown
+## Sous repr√©sentation de la classe unknown
 
 ### Defaut de credit ###
 ech_test$default<-as.factor(ech_test$default)
@@ -122,8 +122,8 @@ freq(ech_test$poutcome)
 ech_test$poutcome<-relevel(ech_test$poutcome,"unknown")
 cram(ech_test$y,ech_test$poutcome)
 ##V=0.3164
-## Attention V cramer ÈlevÈ mais le canal unknown est prÈsent ‡ 81.7% 
-## Si conserver, faire attention ‡ l'individu type
+## Attention V cramer √©lev√© mais le canal unknown est pr√©sent √† 81.7% 
+## Si conserver, faire attention √† l'individu type
 
 ### Classe age ###
 boxplot(ech_test$age)
@@ -132,7 +132,7 @@ levels(ech_test$cl_age)
 freq(ech_test$cl_age)
 cram(ech_test$y,ech_test$cl_age)
 ##V=0.0843
-## Attention ‡ la mise en classe des individus abÈrrants
+## Attention √† la mise en classe des individus ab√©rrants
 
 ## Comparaison cl_age2 ##
 ech_test$cl_age2<-as.factor(ech_test$cl_age2)
@@ -140,7 +140,7 @@ freq(ech_test$cl_age2)
 ech_test$cl_age2<-relevel(ech_test$cl_age2,"2.35-55 ans")
 cram(ech_test$y,ech_test$cl_age2)
 ## V=0.0841
-## Observer si l'une reste ‡ la regression stepwise ##
+## Observer si l'une reste √† la regression stepwise ##
 
 ### Solde du compte ###
 boxplot(ech_test$balance)
@@ -182,7 +182,7 @@ freq(ech_test$cl_duration)
 cram(ech_test$y,ech_test$cl_duration)
 ## V= 0.3563
 
-### Nombre de contact rÈalisÈ ###
+### Nombre de contact r√©alis√© ###
 boxplot(ech_test$campaign)
 ech_test$cl_campaign<-as.factor(ech_test$cl_campaign)
 levels(ech_test$cl_campaign)
@@ -216,7 +216,7 @@ freq(ech_test$cl_month)
 cram(ech_test$y,ech_test$cl_month)
 ## V= 0.09837
 
-### RÈgression logistique et sÈlection du modËle minimisant l'aic ###
+### R√©gression logistique et s√©lection du mod√®le minimisant l'aic ###
 reg <- glm(y ~ 1, 
            data = ech_test, family = binomial(logit))
 model.aic.both <- step(reg, direction = "both", trace = 1, 
@@ -224,20 +224,20 @@ model.aic.both <- step(reg, direction = "both", trace = 1,
                        cl_balance+cl_balance2+cl_day+cl_day2+cl_duration+cl_campaign+cl_pdays+cl_previous+cl_month+cl_job)
 summary(model.aic.both)
 
-### On ne retient que les variables avec une significativitÈ >95% ###
+### On ne retient que les variables avec une significativit√© >95% ###
 reg<-glm(y ~ cl_duration + poutcome + housing +  marital + cl_campaign + loan + cl_day + cl_age2,data=ech_test, family = binomial(logit))
 reg2<-step(reg)
 summary(reg2)
-# Test des rÈsidus studentsÈs # 
+# Test des r√©sidus students√©s # 
 bptest(reg2)
 par(mfrow = c(1, 1))
-plot(rstudent(reg2), type = "p", cex = 0.5, ylab = "RÈsidus studentisÈs ", 
+plot(rstudent(reg2), type = "p", cex = 0.5, ylab = "R√©sidus studentis√©s ", 
      col = "springgreen2", ylim = c(-3, 3))
 abline(h = c(-2, 2), col = "red")
-### Correction de white devra Ítre faÓte afin de rÈgler les problËmes d'htÈroscÈdasticitÈ###
+### Correction de white devra √™tre fa√Æte afin de r√©gler les probl√®mes d'ht√©rosc√©dasticit√©###
 
 ###### ODDS-RATIOS #####
-#### RÈcupÈration des odds ratios ####
+#### R√©cup√©ration des odds ratios ####
 drop1(reg2,.~., test = "Chisq")
 exp(cbind(OR=coef(reg2),confint(reg2)))
 tmp <- tidy(reg2, conf.int = TRUE, exponentiate = TRUE)
@@ -252,11 +252,11 @@ td <- tidy_detailed(reg2, exponentiate = TRUE, conf.int = TRUE)
 td$label <- factor(td$label, rev(td$label))  # Pour fixer l'ordre pour ggplot2
 ggcoef(td, mapping = aes(y = level_detail, x = estimate, 
                          colour = variable_label), exponentiate = TRUE)
-#### RÈsumÈ des odds ratios par variables ####
+#### R√©sum√© des odds ratios par variables ####
 plot(allEffects(reg2))
 summary(reg2)
 
-############ Passage ‡ un modËle avec les dummy pour supprimer de la colinÈaritÈ ############
+############ Passage √† un mod√®le avec les dummy pour supprimer de la colin√©arit√© ############
 ### Observations du taux d'erreur avec les variables dummy ###
 
 regbi<-glm(y ~ Cl_duration_2 +Cl_duration_3 + Cl_duration_4 + Cl_duration_5 +
@@ -268,7 +268,7 @@ regbi<-glm(y ~ Cl_duration_2 +Cl_duration_3 + Cl_duration_4 + Cl_duration_5 +
              Cl_age_1 + Cl_age_3,data=ech_test,family = binomial(logit))
 summary(regbi)
 
-### RÈcupÈration de l'AUC, tpr, fpr###
+### R√©cup√©ration de l'AUC, tpr, fpr###
 proba_predite<-predict(regbi, type="response")
 P<-proba_predite
 pred=prediction(P,ech_test$y)
@@ -278,8 +278,8 @@ sens=performance(pred,"sens")
 auc=performance(pred,"auc")
 plot(perf,colorize = TRUE)
 
-# On choisit le seuil qui minimise la diffÈrence entre la sensibilitÈ et la spÈcificitÈ
-# spÈcificitÈ en valeur absolue
+# On choisit le seuil qui minimise la diff√©rence entre la sensibilit√© et la sp√©cificit√©
+# sp√©cificit√© en valeur absolue
 sensitivity <- sens@y.values[[1]]
 specificity <- spec@y.values[[1]]
 diffspecificity <- 1-specificity
@@ -292,23 +292,23 @@ cutoff <- tab_indicateurs[tab_indicateurs$diff==min(tab_indicateurs$diff),"cutof
 plot(x = tab_indicateurs$cutoff_optimal, y = tab_indicateurs$diff,type='l', 
      main = "cutoff optimal", xlab = "Cutoff", ylab="sensitivity-specificity")
 points(x=cutoff, y=0, pch=16)
-## Diagramme du croisement de la sensibilitÈ et de la spÈcificitÈ ##
+## Diagramme du croisement de la sensibilit√© et de la sp√©cificit√© ##
 plot(y = tab_indicateurs$sensitivity, x = tab_indicateurs$cutoff_optimal , type="l", col = "red", 
      main = "cutoff optimal", xlab = "cutoff", ylab = "")
 lines(y=tab_indicateurs$specificity,x = tab_indicateurs$cutoff_optimal)
 
-## On conserve la valeur optimal au croisement de la courbe sensibilitÈ et spÈcificitÈ ##
+## On conserve la valeur optimal au croisement de la courbe sensibilit√© et sp√©cificit√© ##
 appren.p <- cbind(ech_test, predict(regbi, newdata = ech_test, type = "link", 
                                 se = TRUE))
 
-## RÈcupÈration de la proba prÈdite et de l'IC ##
+## R√©cup√©ration de la proba pr√©dite et de l'IC ##
 appren.p <- within(appren.p, {
   PredictedProb <- plogis(fit)
   LL <- plogis(fit - (1.96 * se.fit))
   UL <- plogis(fit + (1.96 * se.fit))
 })
 
-## On fixe la proba>0.13 croisement spÈcificitÈ et sensibilitÈ ##
+## On fixe la proba>0.13 croisement sp√©cificit√© et sensibilit√© ##
 appren.p <- cbind(appren.p, pred.chd = factor(ifelse(appren.p$PredictedProb > 
                                                        cutoff, 1, 0)))
 m.confusion <- as.matrix(table(appren.p$pred.chd, appren.p$y))
@@ -316,7 +316,7 @@ m.confusion <- unclass(m.confusion)
 # Taux d'erreur
 Tx_err(appren.p$pred.chd, appren.p$y)
 
-############################### Comparaison du modËle sur l'Èchantillon test ##############
+############################### Comparaison du mod√®le sur l'√©chantillon test ##############
 ech_valid<-read_csv("C:/Users/P.Lhostis/Documents/Master/CLASSIFICATION/Base_TEST.csv")
 ech_valid$y<-as.factor(ech_valid$y)
 ech_valid$cl_duration<-as.factor(ech_valid$cl_duration)
@@ -337,8 +337,8 @@ regbi2<-glm(y ~ Cl_duration_2 +Cl_duration_3 + Cl_duration_4 + Cl_duration_5 +
              Cl_day_1 + Cl_day_3 +
              Cl_age_1 + Cl_age_3,data=ech_valid,family = binomial(logit))
 summary(regbi2)
-### Toutes les variables reste significative ‡ plus de 95% ###
-### RÈcupÈration de l'AUC, tpr, fpr###
+### Toutes les variables reste significative √† plus de 95% ###
+### R√©cup√©ration de l'AUC, tpr, fpr###
 proba_predite<-predict(regbi2, type="response")
 P<-proba_predite
 pred=prediction(P,ech_valid$y)
@@ -361,9 +361,9 @@ m.confusion <- unclass(m.confusion)
 # Taux d'erreur
 Tx_err(appren.p$pred.chd, appren.p$y)
 
-### Pas forcÈment pertinent de garder toutes les variables de la reglog ###
-### Cependant, principe de la mÈthode est d'ajoutÈ un maximum de variables et d'utiliser les composantes ##
-### PLS avec toutes les variables suppression de multicolinaritÈ avec rÈgression sur les composantes orthogonales ###
+### Pas forc√©ment pertinent de garder toutes les variables de la reglog ###
+### Cependant, principe de la m√©thode est d'ajout√© un maximum de variables et d'utiliser les composantes ##
+### PLS avec toutes les variables suppression de multicolinarit√© avec r√©gression sur les composantes orthogonales ###
 xpls<-cbind(ech_test$Cl_duration_2 ,ech_test$Cl_duration_3 ,ech_test$Cl_duration_4 ,ech_test$Cl_duration_5,
             ech_test$Poutcome_F,ech_test$Poutcome_O, ech_test$Poutcome_S,
             ech_test$cl_balance24__3000_, ech_test$cl_balance22_100_500_ , ech_test$cl_balance23_500_3000_ , 
@@ -372,22 +372,22 @@ xpls<-cbind(ech_test$Cl_duration_2 ,ech_test$Cl_duration_3 ,ech_test$Cl_duration
             ech_test$Cl_day_1, ech_test$Cl_day_3,
             ech_test$Cl_age_1, ech_test$Cl_age_3)
 ypls<-ech_test$Ynum
-### Observation de notre modËle avec 10 composantes et les variables explicatives ###
+### Observation de notre mod√®le avec 10 composantes et les variables explicatives ###
 res10<-plsRglm(ypls, xpls, nt=10, modele="pls-glm-logistic", pvals.expli=TRUE)
 res.cv.modpls<-cvtable(summary(res10, MClassed = TRUE))
 colSums(res10$pvalstep)
 modpls2 <- plsRglm(dataY=ypls,dataX=xpls, nt = 10, modele = "pls-glm-logistic",sparse=TRUE,
                    sparseStop=TRUE)
 set.seed(123)
-### On cherche ‡ crÈer 2 classes => les oui et les nons ###
-### On rÈalise cela sur 8 composantes orthogonales et on rÈpËte cette action 100 fois afin de s'assurer de la validitÈ de la classification #
+### On cherche √† cr√©er 2 classes => les oui et les nons ###
+### On r√©alise cela sur 8 composantes orthogonales et on r√©p√®te cette action 100 fois afin de s'assurer de la validit√© de la classification #
 cv.modpls.logit<-cv.plsRglm(dataY=ypls,dataX=xpls,nt=8,modele="pls-glm-logistic",K=2,NK=100)
 
 res.cv.modpls.logit=cvtable(summary(cv.modpls.logit, MClassed = TRUE))
 plot(res.cv.modpls.logit)
 res_compl<-plsRglm(ypls, xpls, nt = 8, modele = "pls-glm-logistic",
                    pvals.expli=TRUE)
-### ModËle final ###
+### Mod√®le final ###
 res<-plsRglm(ypls, xpls, nt = 3, modele = "pls-glm-logistic", pvals.expli=TRUE)
 
 res$wwetoile
@@ -401,21 +401,21 @@ spec=performance(pred,"spec")
 sens=performance(pred,"sens")
 auc=performance(pred,"auc")
 plot(perf,colorize = TRUE)
-# spÈcificitÈ en valeur absolue
+# sp√©cificit√© en valeur absolue
 sensitivity <- sens@y.values[[1]]
 specificity <- spec@y.values[[1]]
 diffspecificity <- 1-specificity
 diff <- abs(sensitivity-specificity)
 tab_indicateurs <- data.frame(diff, diffspecificity,cutoff_optimal=spec@x.values[[1]], sensitivity, specificity)
 
-## Recuperation du cutoff optiml pour ma variable prÈdite #
+## Recuperation du cutoff optiml pour ma variable pr√©dite #
 cutoff <- tab_indicateurs[tab_indicateurs$diff==min(tab_indicateurs$diff),"cutoff_optimal"]
 
 
 plot(x = tab_indicateurs$cutoff_optimal, y = tab_indicateurs$diff,type='l', 
      main = "cutoff optimal", xlab = "Cutoff", ylab="sensitivity-specificity")
 points(x=cutoff, y=0, pch=16)
-# Diagramme du croisement de la sensibilitÈ et de la spÈcificitÈ #
+# Diagramme du croisement de la sensibilit√© et de la sp√©cificit√© #
 plot(y = tab_indicateurs$sensitivity, x = tab_indicateurs$cutoff_optimal , type="l", col = "red", 
      main = "cutoff optimal", xlab = "cutoff", ylab = "")
 lines(y=tab_indicateurs$specificity,x = tab_indicateurs$cutoff_optimal)
@@ -428,7 +428,7 @@ m.confusion <- unclass(m.confusion)
 # Taux d'erreur
 Tx_err(appren.p$pred.chd, appren.p$y)
 
-#### Comparaison sur l'Èchantillon test #####
+#### Comparaison sur l'√©chantillon test #####
 xpls2<-cbind(ech_valid$Cl_duration_2 ,ech_valid$Cl_duration_3 ,ech_valid$Cl_duration_4 ,ech_valid$Cl_duration_5,
             ech_valid$Poutcome_F,ech_valid$Poutcome_O, ech_valid$Poutcome_S,
             ech_valid$cl_balance24__3000_, ech_valid$cl_balance22_100_500_ , ech_valid$cl_balance23_500_3000_ , 
@@ -473,9 +473,9 @@ Tx_err(appren.p2$pred.chd, appren.p2$y)
 #fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 20))
 #var <- get_pca_var(res.pca)
 #head(var$cos2, 4)
-# CoordonnÈes
+# Coordonn√©es
 #head(var$coord)
-# Cos2: qualitÈ de rÈpresentation
+# Cos2: qualit√© de r√©presentation
 #head(var$cos2)
 # Contributions aux composantes principales
 #head(var$contrib)
@@ -484,23 +484,23 @@ Tx_err(appren.p2$pred.chd, appren.p2$y)
 #corrplot(var$cos2, is.corr=FALSE)
 # Cos2 total des variables sur Dim.1 et Dim.2
 #fviz_cos2(res.pca, choice = "var", axes = 1:2)
-# Colorer en fonction du cos2: qualitÈ de reprÈsentation
+# Colorer en fonction du cos2: qualit√© de repr√©sentation
 #fviz_pca_var(res.pca, col.var = "cos2",
 #            gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-#             repel = TRUE # …vite le chevauchement de texte
+#             repel = TRUE # √âvite le chevauchement de texte
 #)
 #fviz_pca_var(res.pca, alpha.var = "cos2")
 #head(var$contrib, 4)
 #corrplot(var$contrib, is.corr=FALSE)
-# Contributions des variables ‡ PC1
+# Contributions des variables √† PC1
 #fviz_contrib(res.pca, choice = "var", axes = 1, top = 10)
-# Contributions des variables ‡ PC2
+# Contributions des variables √† PC2
 #fviz_contrib(res.pca, choice = "var", axes = 2, top = 10)
 #fviz_contrib(res.pca, choice = "var", axes = 1:2, top = 10)
 #fviz_pca_var(res.pca, col.var = "contrib",
 #             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07")
 #)
-# CrÈer une variable alÈatoire continue de longueur 10
+# Cr√©er une variable al√©atoire continue de longueur 10
 #set.seed (123)
 #my.cont.var <- rnorm (31649)
 # Colorer les variables en fonction de la variable continue
@@ -513,15 +513,15 @@ Tx_err(appren.p2$pred.chd, appren.p2$y)
 #res.desc$Dim.2
 #ind <- get_pca_ind(res.pca)
 #ind
-# CoordonnÈes des individus
+# Coordonn√©es des individus
 #head(ind$coord)
-# QualitÈ des individus
+# Qualit√© des individus
 #head(ind$cos2)
 # Contributions des individus
 #head(ind$contrib)
 #fviz_pca_ind (res.pca)
 #fviz_pca_ind (res.pca, col.ind = "cos2",
 #              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-#              repel = TRUE # …vite le chevauchement de texte
+#              repel = TRUE # √âvite le chevauchement de texte
 #)
 #############################################################################################
